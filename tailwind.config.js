397fc15c6f8379ad3d48658fde6e78d1dc7092e3
jbouwman7 <jbouwman7@st.noorderpoort.nl>
2022-12-21 15:22:04 +0100

    video en text(text moet beter geplaats worden)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js,css}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#A1C741',
+        'secondary': '#87BD25',
+        'primary-light': '#D0DECA',
+        'white': '#FFFFFF',
+        'gray': '#F2F4F3',
+      },
+      width: {
+        '544': '34rem',
+        '333': '20.8rem',
+      },
+      // fonts: {
+      //   'main': 'montserrat',
+      // },
+    },
   },
   plugins: [],
 }
